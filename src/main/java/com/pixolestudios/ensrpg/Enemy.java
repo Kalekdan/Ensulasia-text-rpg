2581c29914631eb92a3ab46922b250e6f005205c
jrickard <joerickard@hotmail.co.uk>
2019-06-28 09:37:50 +0100

    Made Enemy class abstract

--- src/main/java/com/pixolestudios/ensrpg/Enemy.java
+++ src/main/java/com/pixolestudios/ensrpg/Enemy.java
@@ -1,7 +1,7 @@
 package main.java.com.pixolestudios.ensrpg;
 
-public class Enemy extends NPC {
-    public Enemy(String name, float maxHP){
+public abstract class Enemy extends NPC {
+    protected Enemy(String name, float maxHP){
         super(name, maxHP, true);
     }
 }
